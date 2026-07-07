55d3331e7d1f15bb7ede836a3c5d64add5203624
Dilo1999 <nimeshdilshan440@gmail.com>
2026-07-07 12:34:33 +0530

    Update typography and spacing in CSS, replacing font families with Roboto for consistency. Adjust hero section styles for improved readability and visual hierarchy. Add preconnect links for Google Fonts in the layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,10 @@
 
     {!! \Artesaos\SEOTools\Facades\SEOTools::generate() !!}
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('head')
 </head>
